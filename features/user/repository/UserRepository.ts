2b36da472e067706d2f4184bd8da5ccbc4868439
Tomasz Okniński <okninski.t2004@gmail.com>
2024-02-15 17:33:07 +0100

    Add create user fn to UserRepository

--- features/user/repository/UserRepository.ts
+++ features/user/repository/UserRepository.ts
@@ -1,5 +1,5 @@
 import db from '@/prisma/db'
-
+import bcrypt from 'bcryptjs'
 export class UserRepository {
 	async findUserByEmail(email: string) {
 		return db.user.findUnique({
@@ -12,6 +12,16 @@ export class UserRepository {
 		return db.user.findUnique({
 			where: {
 				id,
+			},
+		})
+	}
+	async createUser(email: string, password: string, name: string) {
+		const hashedPassword = await bcrypt.hash(password, 10)
+		await db.user.create({
+			data: {
+				email,
+				password: hashedPassword,
+				name,
 			},
 		})
 	}
