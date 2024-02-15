0f2aa7301cb5135b7f92cd855252a80b0b683702
Tomasz Okniński <okninski.t2004@gmail.com>
2024-02-15 17:32:32 +0100

    Create UserService

---
+++ features/user/service/UserService.ts
@@ -0,0 +1,34 @@
+import { UserRepository } from './../repository/UserRepository'
+import { RegisterSchema } from '@/features/auth/schemas/RegisterSchema'
+
+export class UserService {
+	async createUser({
+		email,
+		password,
+		name,
+	}: {
+		email: string
+		password: string
+		name: string
+	}) {
+		const userRepository = new UserRepository()
+
+		const validation = RegisterSchema.safeParse({ email, password, name })
+
+		if (!validation.success) {
+			const errorMsgs = validation.error.issues.map(issue => issue.message)
+
+			return { response: { error: errorMsgs.join(', ') }, status: 400 }
+		}
+
+		const existingUser = await userRepository.findUserByEmail(email)
+
+		if (existingUser) {
+			return { response: { error: 'User already exists' }, status: 400 }
+		}
+
+		await userRepository.createUser(email, password, name)
+
+		return { response: { success: 'User created' }, status: 201 }
+	}
+}
